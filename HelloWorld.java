862e0c23e9ab74fd8b51c03453827df7666a6853
Maarten <maarten@ictlab.info>
2012-07-17 14:30:06 +0200

    Added title settters and getters

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,25 @@
 /**
  * Iemand is docs vergeten toe te voegen
+ *
+ * @version 1.0
+ * @author EmVeDee
  */ 
 public class HelloWorld {
+
+   private String sTitle = null;
+
+   public void HelloWorld() {
+       sTitle = "Hello World";
+   }
+
+   public void setTitle(String _title) {
+       sTitle = _title;
+   }
+
+   public String getTitle() {
+      return sTitle;
+   }
+
 	public static void main(String[] arguments) {
 		System.out.println("Hello World!");
 	}
